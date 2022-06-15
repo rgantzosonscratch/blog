11446135b78fd3dbe234ceab80ac229d52688a2f
rgantzosonscratch <rgantzosonscratch@users.noreply.github.com>
2022-06-15 02:13:07 +0000

    deploy: 81b2b355a47cc3704dd880f30d97b22cfbc25b12

--- _nuxt/static/1655259176/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fhosting-vscode-online","\u002F","\u002Fides","\u002Fmongodb-roles","\u002Fnew-projects","\u002Fripple","\u002Fsvelte-mixins","\u002Fuseful-developer-resources","\u002Fyoutube-heatseeker","\u002Fcontact","\u002Fsearch"]})
