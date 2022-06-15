e7a60e60e8d6cb1e3842d24fc50185d5147d8c50
rgantzosonscratch <rgantzosonscratch@users.noreply.github.com>
2022-06-15 02:18:07 +0000

    deploy: 263c0f4c4d8dc7dc7d773225c7be97f71fb77612

--- _nuxt/static/1655259292/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fyoutube-heatseeker","\u002Fcontact","\u002Fsearch"]})
---
+++ _nuxt/static/1655259486/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fhello-world","\u002F","\u002Fsearch","\u002Fcontact"]})
